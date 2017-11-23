2cb2cd30c45d928388779f1216006fd6b78f7ef9
Otavio Augusto <otaviokk@gmail.com>
2017-11-23 19:32:19 -0200

    add redux and dependencies

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import reducers from './rootReducer';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers, composeWithDevTools(
+  applyMiddleware(thunk),
+));
+
+ReactDOM.render((
+  <Provider store={store}>
+    <Router>
+      <Route path="/" component={App} />
+    </Router>
+  </Provider>
+), document.getElementById('root'));
 registerServiceWorker();
