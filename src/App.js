15dd36ed76c641404c470c8c21403e22ca817fb3
Otavio Augusto <otaviokk@gmail.com>
2017-11-24 01:31:34 -0200

    remove unused code

--- src/App.js
+++ src/App.js
@@ -4,17 +4,12 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './home/home';
 import ContentDetail from './content-detail/contentDetail';
 
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header> */}
         <div className="row">
           <Router>
             <div class="col-md-12">
