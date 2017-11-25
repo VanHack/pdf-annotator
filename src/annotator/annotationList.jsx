5cf28e2297ef7b0d3cbb6e25262e1e34960385a3
Otavio Augusto <otaviokk@gmail.com>
2017-11-25 14:44:34 -0200

    clear form after submission

--- src/annotator/annotationList.jsx
+++ src/annotator/annotationList.jsx
@@ -5,7 +5,7 @@ import Moment from 'moment';
 export default ({data}) => (
   <div>
     {data.map((annotation, index) => (
-      <div key={index} class="alert alert-info" role="alert">
+      <div key={index} className="alert alert-info" role="alert">
         <strong>{annotation.name}</strong> <small>posted on {Moment(annotation.date).format('d MMM YYYY')}</small>
         <p>{annotation.notes}</p>
       </div>
